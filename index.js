a5876001aa13a83f86095e6323bde12c11839881
Ashwain1502 <ashwink1502@gamil.com>
2024-07-29 15:23:17 +0530

    ui update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let search = document.getElementById('search');
 let movie = document.getElementById('movie');
 let title = document.getElementById('title');
+let toggleDarkMode = document.getElementById('toggle-dark-mode');
 
 search.addEventListener('click', function() {
     let movieTitle = title.value.trim();
@@ -22,5 +23,15 @@ search.addEventListener('click', function() {
             });
     } else {
         movie.innerHTML = '<p>Please enter a movie title.</p>';
+    }
+});
+
+toggleDarkMode.addEventListener('change', function() {
+    if (this.checked) {
+        document.body.classList.add('dark-mode');
+        document.body.classList.remove('light-mode');
+    } else {
+        document.body.classList.add('light-mode');
+        document.body.classList.remove('dark-mode');
     }
 });
